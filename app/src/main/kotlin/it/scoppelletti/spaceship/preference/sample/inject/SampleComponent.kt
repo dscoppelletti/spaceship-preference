0b9ed35f76df7903251e8641d64444cc2ab6905c
Dario Scoppelletti <dario@scoppelletti.it>
2022-07-04 12:52:37 +0200

    Bump to AGP 7.2.1

--- app/src/main/kotlin/it/scoppelletti/spaceship/preference/sample/inject/SampleComponent.kt
+++ app/src/main/kotlin/it/scoppelletti/spaceship/preference/sample/inject/SampleComponent.kt
@@ -8,11 +8,12 @@ import dagger.Component
 import it.scoppelletti.spaceship.html.inject.HtmlComponent
 import it.scoppelletti.spaceship.html.inject.HtmlModule
 import it.scoppelletti.spaceship.inject.AppComponent
+import it.scoppelletti.spaceship.inject.AppModule
 import it.scoppelletti.spaceship.inject.StdlibComponent
 import javax.inject.Singleton
 
 @Singleton
-@Component
+@Component(modules = [ AppModule::class, HtmlModule::class ])
 interface SampleComponent : AppComponent, HtmlComponent, StdlibComponent {
 
     @Component.Factory
